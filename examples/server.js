41300216a3095f4814847bf27e1aca4e6a6caf8b
wbc <417780894@qq.com>
2021-10-29 00:07:50 +0800

    post请求的data数据处理

--- examples/server.js
+++ examples/server.js
@@ -26,13 +26,18 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 const router = express.Router()
 
-router.get('/simple/get', function (req, res) {
-  res.json({ msg: `hello world` })
+router.get('/simple/get', function (req, res) { res.json({ msg: `hello world` }) })
+router.get('/base/get', function (req, res) { res.json(req.query) })
+router.post('/base/post', function (req, res) { res.json(req.body) })
+router.post('/base/buffer', function (req, res) {
+  let msg = []
+  req.on('data', chunk => { chunk && msg.push(chunk) })
+  req.on('end', () => {
+    let buf = Buffer.concat(msg)
+    res.json(buf.toJSON())
+  })
 })
 
-router.get('/base/get', function (req, res) {
-  res.json(req.query)
-})
 
 app.use(router)
 
--- examples/simple/app.ts
+++ examples/simple/app.ts
@@ -1,43 +1,14 @@
 import axios from '../../src/index'
 
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: { foo: ['bar', 'baz'] }
-})
+// 处理get请求 url参数
+axios({ method: 'get', url: '/base/get', params: { foo: ['bar', 'baz'] } })
+axios({ method: 'get', url: '/base/get', params: { foo: { bar: 'baz' } } })
+axios({ method: 'get', url: '/base/get', params: { date: new Date() } })
+axios({ method: 'get', url: '/base/get', params: { foo: '@:$, ' } })
+axios({ method: 'get', url: '/base/get', params: { foo: 'bar', baz: null } })
+axios({ method: 'get', url: '/base/get#hash', params: { foo: 'bar' } })
+axios({ method: 'get', url: '/base/get?foo=bar', params: { bar: 'baz' } })
 
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: { foo: { bar: 'baz' } }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: { date: new Date() }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: { foo: '@:$, ' }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: { foo: 'bar', baz: null }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get#hash',
-  params: { foo: 'bar' }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get?foo=bar',
-  params: { bar: 'baz' }
-})
+// 处理post请求data为对象时的转化
+axios({ method: 'post', url: '/base/post', data: { a: 1, b: 2 } })
+axios({ method: 'post', url: '/base/buffer', data: new Int32Array([21, 31]) })
